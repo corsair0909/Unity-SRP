7af246ce73462879c43c025dff89ebddc2e2592a
corsair0909 <870233316@qq.com>
2022-08-02 22:37:04 +0800

    Bug修复（Include文件注意顺序）

--- Assets/Shader/UnlitPassCommon.hlsl
+++ Assets/Shader/UnlitPassCommon.hlsl
@@ -2,7 +2,7 @@
 #define CUSTOM_UNLIT_PASS_COMMON_INCLUDE
 #endif
 
-
+#include "Packages/com.unity.render-pipelines.core/ShaderLibrary/Common.hlsl"
 #include "Assets/Shader/UnlitPassInput.hlsl"
 //SpaceTransforms.hlsl代码中没有unity_ObjectToWorld
 //先执行此操作。之后，所有UNITY_MATRIX_M出现都将被unity_ObjectToWorld取代
@@ -13,5 +13,5 @@
 #define UNITY_MATRIX_P glstate_matrix_projection
 
 #include "Packages/com.unity.render-pipelines.core/ShaderLibrary/SpaceTransforms.hlsl"
-#include "Packages/com.unity.render-pipelines.core/ShaderLibrary/Common.hlsl"
+
 
