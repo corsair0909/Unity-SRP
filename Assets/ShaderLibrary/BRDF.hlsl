017f173a0c5149b54d516fcbeea0da2187238e6b
corsair0909 <870233316@qq.com>
2022-08-06 17:58:50 +0800

    BRDF 着色器文件

---
+++ Assets/ShaderLibrary/BRDF.hlsl
@@ -0,0 +1,12 @@
+#ifndef CUSTOM_SURFACE_INCLUDE
+#define CUSTOM_SURFACE_INCLUDE
+struct Surface
+{
+    float3 normal;
+    float3 color;
+    float alpha;
+    float metallic;
+    float smoothness;
+};
+
+#endif
